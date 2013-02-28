9dd3f2ca73ce71fc7679aac253910788083c22f6
Henry Hsiao <me@henrychsiao.com>
2013-02-27 21:37:32 -0800

    Added LoginServices
    Created LoginInfo.java, LoginService.java, LoginServiceAsync.java in
    client and LoginServiceImpl.java in server

---
+++ Judge-Dredd/src/com/google/gwt/judgedredd/client/NotLoggedInException.java
@@ -0,0 +1,15 @@
+package com.google.gwt.judgedredd.client;
+
+import java.io.Serializable;
+
+public class NotLoggedInException extends Exception implements Serializable {
+
+  public NotLoggedInException() {
+    super();
+  }
+
+  public NotLoggedInException(String message) {
+    super(message);
+  }
+
+}
