d38c5ee8da387f5ecff0650492f38c081c75ac45
Henry Hsiao <me@henrychsiao.com>
2013-03-01 00:13:35 -0800

    Data Class
    added Crime.java for data class

---
+++ Judge-Dredd/src/com/google/gwt/judgedredd/server/Crime.java
@@ -0,0 +1,85 @@
+/**
+ * Crime.java
+ * Data Class for Crime
+ * UBC CPSC 310 - Judge Dredd
+ * Team Red Hot Techie Pepper
+ * 
+ * Create Date: February 28, 2013
+ * Last Modified: February 28, 2013
+ */
+
+package com.google.gwt.judgedredd.server;
+
+import java.util.Date;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import com.google.appengine.api.users.User;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Crime {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+	@Persistent
+	private String crimeType;
+	@Persistent
+	private Date crimeDate;
+	@Persistent
+	private Date createdDate;
+	@Persistent
+	private User judge; 
+	@Persistent
+	private Boolean approved;
+	
+	/**
+	 * Constructors 
+	 */
+	public Crime() {
+		this.createdDate = new Date();
+	}
+	
+	public Crime(String type, Date crimeDate, User user){
+		this();
+		this.crimeType = type;
+		this.crimeDate = crimeDate;
+		this.judge = user;
+		this.approved = false;
+	}
+	
+	/**
+	 * Accessors for other classes
+	 */
+	
+	public Long getKey(){
+		return this.id;
+	}
+	
+	public String getType(){
+		return this.crimeType;
+	}
+	
+	public Date getCrimeDate(){
+		return this.crimeDate;
+	}
+	
+	public Date getCreateDate() {
+		return this.createdDate;
+	}
+	
+	public User getJudge(){
+		return this.judge;
+	}
+	
+	public boolean isApproved() {
+		return this.approved;
+	}
+	
+	public void setApproval(){
+		this.approved = true;
+	}
+	
+}
