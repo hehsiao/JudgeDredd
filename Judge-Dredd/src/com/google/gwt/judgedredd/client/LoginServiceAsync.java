60c7863dc49dc8df6415af4d539cdd0ba4c8b24b
Henry Hsiao <me@henrychsiao.com>
2013-03-03 12:58:43 -0800

    Revert "Added LoginServices"
    This reverts commit 9dd3f2ca73ce71fc7679aac253910788083c22f6.

--- Judge-Dredd/src/com/google/gwt/judgedredd/client/LoginServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.google.gwt.judgedredd.client;
-
-import com.google.gwt.judgedredd.client.LoginInfo;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync {
-  public void login(String requestUri, AsyncCallback<LoginInfo> async);
-}
