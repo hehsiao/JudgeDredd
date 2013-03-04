31d6b73e8ca45f94c7854a0c1d2dc5a6ca63a78c
Henry Hsiao <me@henrychsiao.com>
2013-03-03 16:20:04 -0800

    try again ignore images, gwt-unitcache folder

---
+++ JudgeDredd/src/com/google/gwt/judgedredd/client/Matthew.java
@@ -0,0 +1,13 @@
+package com.google.gwt.judgedredd.client;
+
+public class Matthew {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I dont know why");
+	}
+
+}
