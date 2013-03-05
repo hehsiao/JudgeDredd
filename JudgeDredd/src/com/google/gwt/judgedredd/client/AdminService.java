d44600ce30fcbf50eda348d722492693c5ae021e
Matthew Hsu <matthew.hsu@gmail.com>
2013-03-05 13:08:49 -0800

    Approval / Removal Event Handlers Implemented.
    Waiting on Datstore, but basic functionality should be working once it
    is implemented.

--- JudgeDredd/src/com/google/gwt/judgedredd/client/AdminService.java
+++ JudgeDredd/src/com/google/gwt/judgedredd/client/AdminService.java
@@ -8,9 +8,11 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class AdminService implements EntryPoint {
-
+	
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
@@ -18,12 +20,15 @@ public class AdminService implements EntryPoint {
 		
 		FlexTable flexTable = new FlexTable();
 		rootPanel.add(flexTable, 0, 0);
-		flexTable.setSize("450px", "450px");
+		flexTable.setSize("199px", "42px");
 		
-		Label lblTitle = new Label("Dataset(s) Awaiting Approval");
-		flexTable.setWidget(0, 0, lblTitle);
+		Label lblApproval = new Label("Dataset(s) Awaiting Approval");
+		flexTable.setWidget(0, 0, lblApproval);
 		
-		ListBox listBox_Approval = new ListBox();
+		Label lblRemoval = new Label("Dataset(s) Awaiting Removal");
+		flexTable.setWidget(0, 1, lblRemoval);
+		
+		final ListBox listBox_Approval = new ListBox();
 		flexTable.setWidget(1, 0, listBox_Approval);
 		listBox_Approval.setMultipleSelect(true);
 		listBox_Approval.setSize("191px", "290px");
@@ -41,20 +46,74 @@ public class AdminService implements EntryPoint {
 		listBox_Approval.addItem("December");
 		listBox_Approval.setVisibleItemCount(5);
 		
+		final ListBox listBox_Removal = new ListBox();
+		listBox_Removal.setMultipleSelect(true);
+		flexTable.setWidget(1, 1, listBox_Removal);
+		listBox_Removal.setSize("191px", "290px");
+		listBox_Removal.setVisibleItemCount(5);
+		
 		FlexTable flexTable_Button = new FlexTable();
 		flexTable.setWidget(2, 0, flexTable_Button);
 		flexTable_Button.setSize("199px", "42px");
 		
 		Button btnApprove = new Button("Approve");
+		
+		btnApprove.addClickHandler(new ClickHandler()
+		{
+			public void onClick(ClickEvent event) 
+			{
+				for (int index = 0; index < listBox_Approval.getItemCount(); index++)
+				{
+					if (listBox_Approval.isItemSelected(index))
+					{
+						listBox_Removal.addItem(listBox_Approval.getItemText(index));
+						
+						listBox_Approval.removeItem(index);
+						
+						index--;
+					}	// end if
+				}	// end for loop
+			}	// end onClick(ClickEven event)
+		});	// end btnApprove.addClickHandler
+		
 		btnApprove.setText("Approve");
 		flexTable_Button.setWidget(0, 0, btnApprove);
+		btnApprove.setSize("67", "30");
 		flexTable_Button.getCellFormatter().setVerticalAlignment(0, 0, HasVerticalAlignment.ALIGN_MIDDLE);
 		flexTable_Button.getCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
 		flexTable.getCellFormatter().setVerticalAlignment(1, 0, HasVerticalAlignment.ALIGN_MIDDLE);
-		flexTable.getCellFormatter().setHorizontalAlignment(1, 0, HasHorizontalAlignment.ALIGN_CENTER);
-		flexTable.getCellFormatter().setVerticalAlignment(2, 0, HasVerticalAlignment.ALIGN_MIDDLE);
-		flexTable.getCellFormatter().setHorizontalAlignment(2, 0, HasHorizontalAlignment.ALIGN_CENTER);
 		flexTable.getCellFormatter().setVerticalAlignment(0, 0, HasVerticalAlignment.ALIGN_MIDDLE);
 		flexTable.getCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		flexTable.getCellFormatter().setVerticalAlignment(2, 0, HasVerticalAlignment.ALIGN_MIDDLE);
+		flexTable.getCellFormatter().setHorizontalAlignment(1, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		flexTable.getCellFormatter().setHorizontalAlignment(2, 0, HasHorizontalAlignment.ALIGN_CENTER);
+		flexTable.getCellFormatter().setVerticalAlignment(0, 1, HasVerticalAlignment.ALIGN_MIDDLE);
+		flexTable.getCellFormatter().setHorizontalAlignment(0, 1, HasHorizontalAlignment.ALIGN_CENTER);
+		flexTable.getCellFormatter().setVerticalAlignment(1, 1, HasVerticalAlignment.ALIGN_MIDDLE);
+		flexTable.getCellFormatter().setHorizontalAlignment(1, 1, HasHorizontalAlignment.ALIGN_CENTER);
+		
+		Button btnRemoval = new Button("Removal");
+		
+		btnRemoval.addClickHandler(new ClickHandler() 
+		{
+			public void onClick(ClickEvent event) 
+			{
+				for (int index = 0; index < listBox_Removal.getItemCount(); index++)
+				{
+					if (listBox_Removal.isItemSelected(index))
+					{
+						listBox_Approval.addItem(listBox_Removal.getItemText(index));
+						
+						listBox_Removal.removeItem(index);
+						
+						index--;
+					}	// end if
+				}	// end for loop
+			}	// end onClick(ClickEven event)
+		});	// end btnRemoval.addClickHandler
+		
+		flexTable.setWidget(2, 1, btnRemoval);
+		flexTable.getCellFormatter().setVerticalAlignment(2, 1, HasVerticalAlignment.ALIGN_MIDDLE);
+		flexTable.getCellFormatter().setHorizontalAlignment(2, 1, HasHorizontalAlignment.ALIGN_CENTER);
 	}
 }
