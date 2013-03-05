ada4dd046c2aaa99c6a48345dde21ce777eab9b9
Matthew Hsu <matthew.hsu@gmail.com>
2013-03-04 20:39:24 -0800

    More UI Changes.
    Failing at making the HTML into a tabular view. Reverting changes with
    some minor modifications for future work.

--- JudgeDredd/src/com/google/gwt/judgedredd/client/JudgeDredd.java
+++ JudgeDredd/src/com/google/gwt/judgedredd/client/JudgeDredd.java
@@ -41,7 +41,7 @@ public class JudgeDredd implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("Designation...");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
